68da3dad6e930f7be423101a3ee7601de69be489
BrianSP38 <paredes20626@gmail.com>
2026-09-15 15:25:57 -0400

    Add files via upload

---
+++ 3. Gramatica Ambigua/AmbiguaListener.py
@@ -0,0 +1,21 @@
+# Generated from Ambigua.g4 by ANTLR 4.9.2
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .AmbiguaParser import AmbiguaParser
+else:
+    from AmbiguaParser import AmbiguaParser
+
+# This class defines a complete listener for a parse tree produced by AmbiguaParser.
+class AmbiguaListener(ParseTreeListener):
+
+    # Enter a parse tree produced by AmbiguaParser#expr.
+    def enterExpr(self, ctx:AmbiguaParser.ExprContext):
+        pass
+
+    # Exit a parse tree produced by AmbiguaParser#expr.
+    def exitExpr(self, ctx:AmbiguaParser.ExprContext):
+        pass
+
+
+
+del AmbiguaParser
